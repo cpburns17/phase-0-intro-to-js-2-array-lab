2e107b3605bc824e4f425b064b89051e873b54f6
Connor Burns <cpburns17@gmail.com>
2023-11-01 13:56:00 -0400

    complete lab again

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+function appendCat(name) {
+    const moreCats = [...cats, "Broom"];
+    return moreCats;
+}
+function prependCat(name) {
+    const moreCats = ["Arnold", ...cats]
+    return moreCats;
+}
+function removeLastCat(name) {
+    return cats.slice(0,2);
+}
+function removeFirstCat(name) {
+    return cats.slice(1,3);
+}
